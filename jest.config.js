48709a1bc2e706a30084764b06d45742f4e6a56e
Yun Ji <this.jiyun@gmail.com>
2023-02-15 13:06:16 +0100

    chore:jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+process.env['NEXT_PUBLIC_SUPABASE_URL'] = 'https://mkmrcbhppygtzlpeqjar.supabase.co';
+
+process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY'] =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbXJjYmhwcHlndHpscGVxamFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYxMzczOTAsImV4cCI6MTk5MTcxMzM5MH0.Xgf9qF2OtKWu2SiYxKyipx-3a6ISVaXZn4CSGWR9CYA';
+
+export default { transform: {} };
