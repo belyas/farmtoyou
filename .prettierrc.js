7bea2ebd4631d4cf4a903e6a2c8991bbd105e0ad
Yun Ji <this.jiyun@gmail.com>
2023-02-16 11:29:42 +0100

    chore:delete type module declaration

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  quoteProps: 'as-needed',
+  proseWrap: 'never',
+  singleAttributePerLine: true,
+  trailingComma: 'all',
+};
