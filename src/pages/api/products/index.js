f895bcdd80f7a917cca3fb4b497d00f08d24abec
Yun Ji <this.jiyun@gmail.com>
2023-02-20 13:54:40 +0100

    modify api route so it returns all products when there is no query param

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -4,29 +4,36 @@ export default async function products(req, res) {
   if (req.method != 'GET') {
     return res.status(405).json({ data: 'Request method must be GET.' });
   }
+  //if no query param, return all products
 
-  if (!req.query || !req.query.id) {
-    return res.status(400).send({ data: 'Request query must not be empty.' });
-  }
-
-  try {
-    const profileId = req.query.id;
-    let { error, data = [] } = await supabase
-      .from('products')
-      .select('*, farmers ( profile_id )')
-      .eq('farmers.profile_id', profileId);
-
-    // TODO: improve the above query to only return rows related to current farmer
-    if (data.length > 0) {
-      data = data.filter(product => product.farmers?.profile_id === profileId);
+  if (!req.query.id) {
+    try {
+      const { data, error } = await supabase.from('products').select();
+      if (error) {
+        throw typeof error === 'string' ? new Error(error) : error;
+      }
+      return res.status(200).send({ data: data });
+    } catch (error) {
+      return res.status(500).send({ data: 'Internal Server Error.' });
     }
-
-    if (error) {
-      throw typeof error === 'string' ? new Error(error) : error;
-    }
-
-    return res.status(200).json({ data });
-  } catch (error) {
-    return res.status(500).json({ data: 'Internal Server Error.', error });
   }
-}
+  // const farmer_id = parseInt(req.query.id);
+
+  //   if (!Number.isInteger(farmer_id)) {
+  //     return res.status(400).send({ data: 'Id must be an integer' });
+  //   }
+  //   try {
+  //     const { data, error } = await supabase.from('products').select().eq('farmer_id', farmer_id);
+
+  //     if (error) {
+  //       throw typeof error === 'string' ? new Error(error) : error;
+  //     }
+
+  //     if (!data) {
+  //       res.status(404).json({ data: 'not found' });
+  //     }
+  //     res.status(200).json({ data: data });
+  //   } catch (error) {
+  //     return res.status(500).json({ data: 'Internal Server Error.' });
+  //   }
+};
