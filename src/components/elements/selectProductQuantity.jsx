7eed7790942f8c37aca38c98b1c08e9076cad7fb
Yun Ji <this.jiyun@gmail.com>
2023-02-22 13:07:03 +0100

    feat:dynamically generate quantity select options within product stock range

--- src/components/elements/selectProductQuantity.jsx
+++ src/components/elements/selectProductQuantity.jsx
@@ -1,13 +1,16 @@
-import { Typography, Select, FormControl, InputLabel, MenuItem } from '@mui/material';
+import { Select, FormControl, InputLabel, MenuItem } from '@mui/material';
 import { useState } from 'react';
 
-const SelectProductQuantity = () => {
+const SelectProductQuantity = ({ stock }) => {
   const [quantity, SetQuantity] = useState(1);
+  const maxQuantity = stock < 10 ? stock : 10;
+
+  const quantityOptions = [...Array(maxQuantity).keys()].map(n => n + 1);
 
   const handleChange = event => {
     SetQuantity(event.target.value);
   };
-  //todo select option shall not exceed the quantity of a given product
+
   return (
     <FormControl sx={{ m: 1, minWidth: 120 }}>
       <InputLabel id="product-amount-select-label">Quantity</InputLabel>
@@ -18,15 +21,16 @@ const SelectProductQuantity = () => {
         label="quantity"
         onChange={handleChange}
       >
-        <MenuItem value={1}>1</MenuItem>
-        <MenuItem value={2}>2</MenuItem>
-        <MenuItem value={3}>3</MenuItem>
-        <MenuItem value={4}>4</MenuItem>
-        <MenuItem value={5}>5</MenuItem>
-        <MenuItem value={6}>6</MenuItem>
-        <MenuItem value={7}>7</MenuItem>
-        <MenuItem value={8}>8</MenuItem>
-        <MenuItem value={9}>9</MenuItem>
+        {quantityOptions.map((num, index) => {
+          return (
+            <MenuItem
+              key={index}
+              value={num}
+            >
+              {num}
+            </MenuItem>
+          );
+        })}
       </Select>
     </FormControl>
   );
