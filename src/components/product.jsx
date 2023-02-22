6d728d7fde8a01bd68e2524d4c32103ec8c44814
Yun Ji <this.jiyun@gmail.com>
2023-02-22 09:56:17 +0100

    commit before pull from main

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,6 +1,55 @@
-const Product = productData => {
+import Grid from '@mui/material/Unstable_Grid2';
+import Image from 'next/image';
+import { Typography } from '@mui/material';
+
+const Product = ({ productData }) => {
   console.log('productdata', productData);
-  return <>product found</>;
+  console.log('product title', productData.title);
+  const product = productData[0];
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={2}
+      >
+        <Grid
+          xs={12}
+          md={6}
+        >
+          <Image
+            src="/images/veggie-basket.jpg"
+            title="placeholder veggie-basket"
+            height={500}
+            width={500}
+          />
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+        >
+          <Typography
+            gutterBottom
+            variant="h3"
+          >
+            {product.title}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="h5"
+          >
+            € {product.price}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+          >
+            {product.description}
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Product;
