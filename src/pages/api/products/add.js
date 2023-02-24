b0056311a64cd24980538ac378fd51b98e0dc478
Yun Ji <this.jiyun@gmail.com>
2023-02-24 13:06:35 +0100

    fix:construct product object instead of mutating body object

--- src/pages/api/products/add.js
+++ src/pages/api/products/add.js
@@ -3,8 +3,11 @@ import hasEmptyValue from '@/utils/hasEmptyValue';
 import hasAllFields from '@/utils/hasAllFields';
 import formidable from 'formidable';
 import path from 'path';
-import cleanData from '@/utils/cleanProductData';
 import fs from 'fs';
+import correctDBArray from '@/utils/isCorrectDBArrayFormat';
+import farmerFound from '@/utils/findFarmerById';
+import validDate from '@/utils/isValidDate';
+import isoDate from '@/utils/isIsoDate';
 
 export default async function add(req, res) {
   if (req.method != 'POST') {
@@ -39,18 +42,15 @@ export default async function add(req, res) {
     }
     const photo = files['photo'].newFilename;
 
-    req.body = fields;
+    const product = {};
 
-    //asign photo name to body data and pass it to db
-    req.body.photo = photo;
+    product.photo = photo;
 
-    const isEmptyBody = Object.keys(req.body).length === 0 ? true : false;
+    const isEmptyBody = Object.keys(fields).length === 0 ? true : false;
 
     if (isEmptyBody) {
       return res.status(400).json({ data: 'Request body must not be empty' });
     }
-
-    const body = req.body;
 
     //Need to have all required fields
     const formFields = [
@@ -69,20 +69,85 @@ export default async function add(req, res) {
       'delivery_method',
     ];
 
-    if (!hasAllFields(Object.keys(body), formFields)) {
+    if (!hasAllFields(Object.keys(fields), formFields)) {
       return res.status(400).json({ data: 'Need to have all required fields.' });
     }
 
-    const emptyValue = hasEmptyValue(Object.values(body));
+    const emptyValue = hasEmptyValue(Object.values(fields));
 
     if (emptyValue) {
       return res.status(400).json({ data: 'All fields must not be empty' });
     }
 
-    cleanData(body, res);
+    //clean description, title and photo strings
+    product.description = fields.description.trim();
+    product.title = fields.title.trim();
+    product.photo = fields.photo.trim();
+
+    //check date is in ISO format && is in future &&  end>start
+    if (!isoDate(fields.subscription_start) || isoDate(fields.subscription_end)) {
+      return res.status(400).json({ data: 'Date must be in ISO format,e.g.,2023-01-01' });
+    }
+    const isValidDate = validDate(fields.subscription_start, fields.subscription_end);
+    if (!isValidDate.valid) {
+      return res.status(400).json({ data: isValidDate.message });
+    }
+    product.subscription_start = fields.subscription_start;
+    product.subscription_end = fields.subscription_end;
+
+    //check price and quantity are postive Int
+    const priceIsPositiveInt = Math.sign(fields.price) === 1;
+    const quantityIsPositiveInt = Math.sign(fields.quantity) === 1;
+
+    if (!priceIsPositiveInt) {
+      return res.status(400).json({ data: 'Price must be a positive integer' });
+    }
+    if (!quantityIsPositiveInt) {
+      return res.status(400).json({ data: 'Quantity must be a positive integer' });
+    }
+    product.price = parseInt(fields.price);
+    product.quantity = parseInt(fields.quantity);
+
+    //check farmer exists in db and parse id into Int
+    const farmerExists = await farmerFound(parseInt(fields.farmer_id));
+    if (!farmerExists) {
+      return res.status(400).json({ data: 'Farmer id must be valid' });
+    }
+    product.farmer_id = parseInt(fields.farmer_id);
+
+    //check delivery date is within a day of a week and parse delivery_date to lowercase
+    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const deliveryDateInWeek =
+      daysOfWeek.findIndex(day => day.toLowerCase() === fields.delivery_date.toLowerCase()) !== -1;
+    if (!deliveryDateInWeek) {
+      return res.status(400).json({ data: 'Delivery date must be a day of a week' });
+    }
+    product.delivery_date = fields.delivery_date.toLowerCase();
+
+    if (parseInt(fields.subscription_frequency) !== 1) {
+      return res.status(400).json({ data: 'Subscription frequency must be 1' });
+    }
+    product.subscription_frequency = parseInt(fields.subscription_frequency);
+
+    //check if organic field is boolean and parse it to boolean
+    if (fields.organic.toLowerCase() === 'true') {
+      fields.organic = true;
+    } else if (fields.organic.toLowerCase() === 'false') {
+      fields.organic = false;
+    } else {
+      return res.status(400).json({ data: 'Organic field must provide a boolean value' });
+    }
+    product.organic = fields.organic;
+
+    product.delivery_method = correctDBArray(fields.delivery_method)
+      ? fields.delivery_method
+      : '{' + fields.delivery_method + '}';
+    product.category = correctDBArray(fields.category) ? fields.category : '{' + fields.category + '}';
+
+    console.log(product);
 
     try {
-      const { error } = await supabase.from('products').insert(body);
+      const { error } = await supabase.from('products').insert(product);
       if (error) {
         throw typeof error === 'string' ? new Error(error) : error;
       }
