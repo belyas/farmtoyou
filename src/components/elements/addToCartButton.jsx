51ef77e363079aa78d261fda566adbdd564a26c0
Yun Ji <this.jiyun@gmail.com>
2023-02-25 10:55:58 +0100

    Feat:attach handler to add to cart button

--- src/components/elements/addToCartButton.jsx
+++ src/components/elements/addToCartButton.jsx
@@ -1,6 +1,18 @@
 import Button from '@mui/material/Button';
 
 const AddToCartButton = () => {
-  return <Button variant="contained">Add to cart</Button>;
+  const handleClick = e => {
+    console.log(e);
+  };
+  return (
+    <Button
+      variant="contained"
+      onClick={e => {
+        handleClick(e);
+      }}
+    >
+      Add to cart
+    </Button>
+  );
 };
 export default AddToCartButton;
