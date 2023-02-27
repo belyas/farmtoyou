0aafbfed3517c1cbcc96f6fb5cda77f97c714340
Yun Ji <this.jiyun@gmail.com>
2023-02-27 16:23:27 +0100

    use MUI badget

--- src/components/cart/shoppingCart.js
+++ src/components/cart/shoppingCart.js
@@ -1,7 +1,7 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { CartContext } from './cartContext';
 import { useContext } from 'react';
-import Badge from '@mui/joy/Badge';
+import Badge from '@mui/material/Badge';
 
 const ShoppingCart = () => {
   const { cart } = useContext(CartContext);
