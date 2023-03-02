ff4fd0a5d8313fb71f5fc7eab0195e1f50c0e9ce
Yun Ji <this.jiyun@gmail.com>
2023-03-02 10:27:53 +0100

    feat:add conditions to orders page.If no orders,show no order message and provide link to go back to home page

--- src/pages/orders/index.jsx
+++ src/pages/orders/index.jsx
@@ -8,6 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Link from 'next/link';
+import { Typography } from '@mui/material';
 
 export const getServerSideProps = async ctx => {
   const supabase = createServerSupabaseClient(ctx);
@@ -37,40 +38,50 @@ export const getServerSideProps = async ctx => {
 
 const Orders = ({ orders }) => {
   console.log(orders);
+
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table
-          sx={{ minWidth: 650 }}
-          aria-label="orders table"
-        >
-          <TableHead>
-            <TableRow>
-              <TableCell>Order id</TableCell>
-              <TableCell align="right">Created at</TableCell>
-              <TableCell align="right">Total Amount</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orders.map((order, index) => (
-              <TableRow
-                key={index}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell
-                  component="th"
-                  scope="row"
-                >
-                  <Link href={`/orders/${order.id}`}>{order.id}</Link>
-                </TableCell>
-
-                <TableCell align="right">{order.created_at}</TableCell>
-                <TableCell align="right">{order.total_amount}</TableCell>
+      {orders.length ? (
+        <TableContainer component={Paper}>
+          <Table
+            sx={{ minWidth: 650 }}
+            aria-label="orders table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell>Order id</TableCell>
+                <TableCell align="right">Created at</TableCell>
+                <TableCell align="right">Total Amount</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {orders.map((order, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell
+                    component="th"
+                    scope="row"
+                  >
+                    <Link href={`/orders/${order.id}`}>{order.id}</Link>
+                  </TableCell>
+
+                  <TableCell align="right">{order.created_at}</TableCell>
+                  <TableCell align="right">{order.total_amount}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <div>
+          <Typography variant="h5">You have not ordered anything yet. </Typography>
+          <Link href="/">
+            <Typography variant="body1">Back to home</Typography>
+          </Link>
+        </div>
+      )}
     </>
   );
 };
