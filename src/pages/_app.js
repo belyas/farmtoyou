f42cb8115fce5c4dc2f94dc2c65eccb6ca9fb88a
yassine-belkaid <yassine.belkaid@adevinta.com>
2023-03-05 00:06:38 +0100

    feat: add layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,40 +2,32 @@ import '@/styles/globals.css';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { CartProvider } from '@/components/cart/cartContext';
-import ShoppingCart from '@/components/cart/shoppingCart';
-import ClearCartButton from '@/components/cart/clearCart';
+import { Header } from '@/components/layout';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4e7c5f',
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
-  const route = useRouter();
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
-      <CartProvider>
-        {!pageProps?.initialSession && <Link href={'/login'}>Login</Link>}
-        {pageProps?.initialSession && (
-          <>
-            <button
-              onClick={async () => {
-                await supabase.auth.signOut();
-                route.push('/');
-              }}
-            >
-              Sign Out
-            </button>{' '}
-            | <Link href={'/profile'}>Profile</Link> | <Link href={'/products'}>Products</Link>
-          </>
-        )}
-        <Component {...pageProps} />
-        <ShoppingCart />
-        <ClearCartButton />
-      </CartProvider>
-    </SessionContextProvider>
+    <ThemeProvider theme={theme}>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <CartProvider>
+          <Header />
+          <Component {...pageProps} />
+        </CartProvider>
+      </SessionContextProvider>
+    </ThemeProvider>
   );
 }
