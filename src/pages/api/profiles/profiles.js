d1c2fcd12a1663a46896c5b0b27df2fa8644809c
odda1991 <96075423+odda1991@users.noreply.github.com>
2023-03-04 18:58:49 +0100

    update profile

--- src/pages/api/profiles/profiles.js
+++ src/pages/api/profiles/profiles.js
@@ -1,30 +1,31 @@
 import { supabase } from '../../../utils/supabaseClient';
+import {update} from '../profiles/updateProfile'
 
 export default async function getProfile(req, res) {
   if (req.method !== 'PUT') {
-    return res.status(405).send({ data: 'Request must be PUT' });
+  return res.status(405).send({ data: 'Request must be PUT' });
   }
   if (!req.body) {
     return res.status(400).send({ data: 'Request must not be empty.' });
   }
   const profile_id = req.body;
 
-  try {
-    const { data, error } = await supabase
-      .from('farmers')
-      .update(body)
-      .select('*, profiles!inner (*)')
-      .eq('profile_id', profile_id)
-      .single();
-    if (error) {
-      throw typeof error === 'string' ? new Error(error) : error;
-    }
+    try {
+        const { data, error } = await supabase
+        .from('farmers')
+        .update(body)
+        .select('*, profiles!inner (*)')
+        .eq('profile_id', profile_id)
+        .single();
+        if (error) {
+        throw typeof error === 'string' ? new Error(error) : error;
+        }
 
-    if (!data) {
-      res.status(404).json({ data: 'not found' });
+        if (!data) {
+        res.status(404).json({ data: 'not found' });
+        }
+        res.status(200).json({ data: data });
+    } catch (error) {
+        return res.status(500).json({ data: 'Internal Server Error.', error });
     }
-    res.status(200).json({ data: data });
-  } catch (error) {
-    return res.status(500).json({ data: 'Internal Server Error.', error });
-  }
 }
