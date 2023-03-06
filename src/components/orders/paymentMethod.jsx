744a8399bcb240c44b14644316349ad0c84a4375
Yun Ji <this.jiyun@gmail.com>
2023-03-06 10:01:24 +0100

    fix: only show last four digits of card number

--- src/components/orders/paymentMethod.jsx
+++ src/components/orders/paymentMethod.jsx
@@ -1,10 +1,12 @@
 import { Typography } from '@mui/material';
 
 const PaymentMethod = ({ order }) => {
+  console.log(typeof order.card_number);
+  const cardNumber = '*'.repeat(12) + order.card_number.toString().substring(12);
   return (
     <>
-      <Typography variant="subtitle1">{order.card_holder}</Typography>
-      <Typography variant="body1">{order.card_number}</Typography>
+      <Typography variant="subtitle1">{order.card_holder.toUpperCase()}</Typography>
+      <Typography variant="body1">{cardNumber}</Typography>
       <Typography variant="body1">Expiration Date:{order.expiration_date} </Typography>
       <Typography variant="body1">CVV:{order.card_cvv}</Typography>
     </>
