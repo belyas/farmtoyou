310573a29fd3fd40ff00714c347f261c73e186e4
Yun Ji <this.jiyun@gmail.com>
2023-03-06 10:02:45 +0100

    fix:check if data is null

--- src/utils/getFarmerId.js
+++ src/utils/getFarmerId.js
@@ -2,16 +2,14 @@ import { supabase } from './supabaseClient';
 
 export default async function isUserFarmer(id) {
   try {
-    const { data, error } = await supabase.from('farmer_profile').select('id').eq('profile_id', id).single();
-
+    const { data, error } = await supabase.from('farmer_profile').select('id').eq('profile_id', id).maybeSingle();
     if (error) {
       throw typeof error === 'string' ? new Error(error) : error;
     }
-    //if not found in db, an error will be thrown
+    //if not found in db, data is null
     //if found, db returns {id:15}(example)
-
-    return data.id;
+    return data?.id;
   } catch (error) {
-    return false;
+    return error;
   }
 }
