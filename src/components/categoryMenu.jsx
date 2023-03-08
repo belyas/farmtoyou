8564e9e984b7c1c769838f36c413a7f48913fff7
Yun Ji <this.jiyun@gmail.com>
2023-03-08 11:26:46 +0100

    delete unused imports

--- src/components/categoryMenu.jsx
+++ src/components/categoryMenu.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
 import CategoryList from './categoryList';
 
