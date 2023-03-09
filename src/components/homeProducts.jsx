ba7e2efd69dabf5fe7849636554368ab780574d5
Yun Ji <this.jiyun@gmail.com>
2023-03-09 13:39:35 +0100

    style:center add to cart button

--- src/components/homeProducts.jsx
+++ src/components/homeProducts.jsx
@@ -16,7 +16,7 @@ const HomeProducts = ({ productsData }) => {
   const productItems = products.map((product, index) => {
     return (
       <Grid
-        xs={2}
+        xs={12}
         sm={4}
         md={4}
         key={index}
@@ -24,7 +24,7 @@ const HomeProducts = ({ productsData }) => {
         <Card sx={{ maxWidth: 345 }}>
           <Link href={`/products/${product.id}`}>
             <CardMedia
-              sx={{ height: 140, backgroundSize: 'contain' }}
+              sx={{ height: 140, backgroundSize: 'contain', my: 1 }}
               image={
                 product.photo && product.photo.endsWith('.jpg')
                   ? `/uploads/products/${product.photo}`
@@ -52,7 +52,7 @@ const HomeProducts = ({ productsData }) => {
             </Typography>
           </CardContent>
 
-          <CardActions>
+          <CardActions sx={{ justifyContent: 'center' }}>
             <AddToCartButton
               product={product}
               quantity={1}
