79e378981ddf613d830fe7bce4302449e32524a6
Ali Murtaza <syedalim9087@gmail.com>
2023-03-10 13:57:51 +0100

    Implemeneted Product Layout

--- src/pages/products/add.jsx
+++ src/pages/products/add.jsx
@@ -8,6 +8,7 @@ import * as Yup from 'yup';
 import { Alert } from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
 import { getURL } from '@/utils';
+import styles from '@/styles/add.module.css';
 
 export async function getServerSideProps(ctx) {
   const supabase = createServerSupabaseClient(ctx);
@@ -173,291 +174,344 @@ const Add = ({ data, error }) => {
   };
 
   return (
-    <form
-      action="/api/products/add"
-      method="post"
-      onSubmit={formik.handleSubmit}
-      encType="multipart/form-data"
-      noValidate
-    >
-      <Snackbar
-        open={showError}
-        autoHideDuration={3000}
-        onClose={() => setShowError(false)}
+    <div >
+      <form
+        action="/api/products/add"
+        method="post"
+        onSubmit={formik.handleSubmit}
+        encType="multipart/form-data"
+        noValidate
+        className={styles.formGrid}
       >
-        <Alert severity="error">Failed to submit data</Alert>
-      </Snackbar>
-      <Snackbar
-        open={showSuccess}
-        autoHideDuration={3000}
-        onClose={() => setShowSuccess(false)}
-      >
-        <Alert severity="success">Successfully submitted data</Alert>
-      </Snackbar>
-      <div>
-        <label
-          htmlFor="title"
-          id="title"
+        <Snackbar
+          open={showError}
+          autoHideDuration={3000}
+          onClose={() => setShowError(false)}
         >
-          Title:{' '}
-          {formik.touched.title && formik.errors.title ? (
-            <span style={{ color: 'red' }}>{formik.errors.title}</span>
-          ) : (
-            ''
-          )}
-        </label>
-        <input
-          type="text"
-          name="title"
-          required
-          value={formik.values.title}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-      </div>
-      <div>
-        <label htmlFor="description">
-          Description:{' '}
-          {formik.touched.description && formik.errors.description ? (
-            <span style={{ color: 'red' }}>{formik.errors.description} </span>
-          ) : (
-            ''
-          )}
-        </label>
-        <input
-          type="text"
-          name="description"
-          required
-          value={formik.values.description}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-      </div>
-      <div>
-        <label htmlFor="price">
-          Price:{' '}
-          {formik.touched.price && formik.errors.price ? (
-            <span style={{ color: 'red' }}>{formik.errors.price} </span>
-          ) : (
-            ''
-          )}
-        </label>
-        <input
-          type="number"
-          name="price"
-          required
-          value={formik.values.price}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-      </div>
-      <div>
-        <label htmlFor="quantity">
-          Quantity:{' '}
-          {formik.touched.quantity && formik.errors.quantity ? (
-            <span style={{ color: 'red' }}>{formik.errors.quantity} </span>
-          ) : (
-            ''
-          )}
-        </label>
-        <input
-          type="number"
-          name="quantity"
-          required
-          value={formik.values.quantity}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="subscription_start">
-          Subscription Start:{' '}
-          {formik.touched.subscription_start && formik.errors.subscription_start ? (
-            <span style={{ color: 'red' }}>{formik.errors.subscription_start}</span>
-          ) : (
-            ''
-          )}
-        </label>
-        <input
-          type="date"
-          name="subscription_start"
-          required
-          onChange={event => handleSubStartChange(new Date(event.target.value))}
-          value={formik.values.subscription_start}
-          onBlur={formik.handleBlur}
-        />
-      </div>
-      <div>
-        <label htmlFor="subscription_end">
-          Subcription End:
-          {formik.touched.subscription_end && formik.errors.subscription_end ? (
-            <span style={{ color: 'red' }}>{formik.errors.subscription_end}</span>
-          ) : (
-            ''
-          )}
-        </label>
-        <input
-          type="date"
-          name="subscription_end"
-          required
-          onChange={event => handleSubEndChange(new Date(event.target.value))}
-          value={formik.values.subscription_end}
-          onBlur={formik.handleBlur}
-        />
-      </div>
-      <div>
-        <label htmlFor="file">
-          Photo:
-          {formik.touched.photo && formik.errors.photo && <span style={{ color: 'red' }}>{formik.errors.photo}</span>}
-        </label>
-        <input
-          type="file"
-          name="photo"
-          onBlur={formik.handleBlur}
-          onChange={handlePhotoChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="delivey_date">
-          Delivery Date:
-          {formik.touched.delivery_date && formik.errors.delivery_date ? (
-            <span style={{ color: 'red' }}>{formik.errors.delivery_date}</span>
-          ) : (
-            ''
-          )}
-        </label>
-        <select
-          id="dayOfWeek"
-          name="dayOfWeek"
-          value={formik.values.delivery_date}
-          label="Select the Day"
-          onBlur={formik.handleBlur}
-          onChange={event => {
-            formik.setFieldValue('delivery_date', event.target.value);
-          }}
+          <Alert severity="error">Failed to submit data</Alert>
+        </Snackbar>
+        <Snackbar
+          open={showSuccess}
+          autoHideDuration={3000}
+          onClose={() => setShowSuccess(false)}
         >
-          {' '}
-          <option
-            value=""
-            disabled
-          >
-            Select the day
-          </option>
-          {daysOfWeek.map(day => (
-            <option
-              key={day}
-              value={day}
-            >
-              {day}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="subscription_frequency">
-          Subscription Frequency:
-          {formik.touched.subscription_frequency && formik.errors.subscription_frequency ? (
-            <span style={{ color: 'red' }}>{formik.errors.subscription_frequency}</span>
-          ) : (
-            ''
-          )}
-        </label>
-        <select
-          id="subscription_frequency"
-          name="subscription_frequency"
-          value={formik.values.subscription_frequency}
-          onChange={handleFrequency}
-          onBlur={formik.handleBlur}
-        >
-          <option value="Select an option">Select an option</option>
-          <option value={1}>Once a week</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="delivery_method">
-          Delivery method:
-          {formik.touched.delivery_method && formik.errors.delivery_method ? (
-            <span style={{ color: 'red' }}>{formik.errors.delivery_method}</span>
-          ) : (
-            ''
-          )}
-        </label>
-        <select
-          id="delivery_method"
-          name="delivery_method"
-          value={formik.values.delivery_method}
-          onChange={handleDeliveryMethod}
-          onBlur={formik.handleBlur}
-        >
-          <option
-            value=""
-            disabled
-          >
-            Select an option
-          </option>
-          <option value={1}>Farmer delivery</option>
-          <option value={2}>Pick up place</option>
-        </select>
-      </div>
-
-      <div>
-        {formik.touched.category && formik.errors.category && (
-          <div style={{ color: 'red' }}>{formik.errors.category}</div>
-        )}
-        <Autocomplete
-          multiple
-          id="category"
-          options={category}
-          sx={{ width: 300, margin: 1 }}
-          value={formik.values.category}
-          onBlur={formik.handleBlur}
-          onChange={(event, value) => {
-            formik.setFieldValue('category', value);
-          }}
-          renderInput={params => (
-            <TextField
-              {...params}
-              label="category"
-              variant="outlined"
-              fullWidth
-            />
-          )}
-        />
-      </div>
-      <div>
-        <label>Is it organic?</label>
+          <Alert severity="success">Successfully submitted data</Alert>
+        </Snackbar>
         <div>
-          <label>
-            <input
-              type="radio"
-              name="organic"
-              value="Yes"
-              checked={formik.values.organic === 'Yes'}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            Yes
+          <label
+            htmlFor="title"
+            id="title"
+            className={styles.label}
+          >
+            Title:{' '}
+            {formik.touched.title && formik.errors.title ? (
+              <span style={{ color: 'red' }}>{formik.errors.title}</span>
+            ) : (
+              ''
+            )}
           </label>
-          <label>
-            <input
-              type="radio"
-              name="organic"
-              value="No"
-              checked={formik.values.organic === 'No'}
-              onChange={formik.handleChange}
-            />
-            No
-          </label>
+          <input
+            type="text"
+            name="title"
+            required
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={styles.input}
+          />
         </div>
-        {formik.touched.organic && formik.errors.organic ? (
-          <div style={{ color: 'red' }}>{formik.errors.organic}</div>
-        ) : null}
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+        <div>
+          <label
+            htmlFor="description"
+            className={styles.label}
+          >
+            Description:{' '}
+            {formik.touched.description && formik.errors.description ? (
+              <span style={{ color: 'red' }}>{formik.errors.description} </span>
+            ) : (
+              ''
+            )}
+          </label>
+          <input
+            type="text"
+            name="description"
+            required
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="price"
+            className={styles.label}
+          >
+            Price:{' '}
+            {formik.touched.price && formik.errors.price ? (
+              <span style={{ color: 'red' }}>{formik.errors.price} </span>
+            ) : (
+              ''
+            )}
+          </label>
+          <input
+            type="number"
+            name="price"
+            required
+            value={formik.values.price}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="quantity"
+            className={styles.label}
+          >
+            Quantity:{' '}
+            {formik.touched.quantity && formik.errors.quantity ? (
+              <span style={{ color: 'red' }}>{formik.errors.quantity} </span>
+            ) : (
+              ''
+            )}
+          </label>
+          <input
+            type="number"
+            name="quantity"
+            required
+            value={formik.values.quantity}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className={styles.input}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="subscription_start"
+            className={styles.label}
+          >
+            Subscription Start:{' '}
+            {formik.touched.subscription_start && formik.errors.subscription_start ? (
+              <span style={{ color: 'red' }}>{formik.errors.subscription_start}</span>
+            ) : (
+              ''
+            )}
+          </label>
+          <input
+            type="date"
+            name="subscription_start"
+            required
+            onChange={event => handleSubStartChange(new Date(event.target.value))}
+            value={formik.values.subscription_start}
+            onBlur={formik.handleBlur}
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subscription_end"
+            className={styles.label}
+          >
+            Subcription End:
+            {formik.touched.subscription_end && formik.errors.subscription_end ? (
+              <span style={{ color: 'red' }}>{formik.errors.subscription_end}</span>
+            ) : (
+              ''
+            )}
+          </label>
+          <input
+            type="date"
+            name="subscription_end"
+            required
+            onChange={event => handleSubEndChange(new Date(event.target.value))}
+            value={formik.values.subscription_end}
+            onBlur={formik.handleBlur}
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="file"
+            className={styles.label}
+          >
+            Photo:
+            {formik.touched.photo && formik.errors.photo && <span style={{ color: 'red' }}>{formik.errors.photo}</span>}
+          </label>
+          <input
+            type="file"
+            name="photo"
+            onBlur={formik.handleBlur}
+            onChange={handlePhotoChange}
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="delivey_date"
+            className={styles.label}
+          >
+            Delivery Date:
+            {formik.touched.delivery_date && formik.errors.delivery_date ? (
+              <span style={{ color: 'red' }}>{formik.errors.delivery_date}</span>
+            ) : (
+              ''
+            )}
+          </label>
+          <select
+            id="dayOfWeek"
+            name="dayOfWeek"
+            value={formik.values.delivery_date}
+            label="Select the Day"
+            onBlur={formik.handleBlur}
+            onChange={event => {
+              formik.setFieldValue('delivery_date', event.target.value);
+            }}
+            className={styles.selectTag}
+          >
+            {' '}
+            <option
+              value=""
+              disabled
+            >
+              Select the day
+            </option>
+            {daysOfWeek.map(day => (
+              <option
+                key={day}
+                value={day}
+              >
+                {day}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="subscription_frequency"
+            className={styles.label}
+          >
+            Subscription Frequency:
+            {formik.touched.subscription_frequency && formik.errors.subscription_frequency ? (
+              <span style={{ color: 'red' }}>{formik.errors.subscription_frequency}</span>
+            ) : (
+              ''
+            )}
+          </label>
+          <select
+            id="subscription_frequency"
+            name="subscription_frequency"
+            value={formik.values.subscription_frequency}
+            onChange={handleFrequency}
+            onBlur={formik.handleBlur}
+            className={styles.selectTag}
+          >
+            <option value="Select an option">Select an option</option>
+            <option value={1}>Once a week</option>
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="delivery_method"
+            className={styles.label}
+          >
+            Delivery method:
+            {formik.touched.delivery_method && formik.errors.delivery_method ? (
+              <span style={{ color: 'red' }}>{formik.errors.delivery_method}</span>
+            ) : (
+              ''
+            )}
+          </label>
+          <select
+            id="delivery_method"
+            name="delivery_method"
+            value={formik.values.delivery_method}
+            onChange={handleDeliveryMethod}
+            onBlur={formik.handleBlur}
+            className={styles.selectTag}
+          >
+            <option
+              value=""
+              disabled
+            >
+              Select an option
+            </option>
+            <option value={1}>Farmer delivery</option>
+            <option value={2}>Pick up place</option>
+          </select>
+        </div>
+
+        <div>
+          <label className={styles.label}>
+            Select a catagory:
+            {formik.touched.category && formik.errors.category && (
+              <span style={{ color: 'red' }}>{formik.errors.category}</span>
+            )}
+          </label>
+          <Autocomplete
+          className={styles.autocomplete}
+            multiple
+            id="category"
+            options={category}
+            sx={{ width: 690, height: 42 }}
+            value={formik.values.category}
+            onBlur={formik.handleBlur}
+            onChange={(event, value) => {
+              formik.setFieldValue('category', value);
+            }}
+            renderInput={params => (
+              <TextField
+                {...params}
+                label="category"
+                variant="outlined"
+                fullWidth
+                sx={{backgroundColor:"var(--custom-bg-color)"}}
+              />
+            )}
+          />
+        </div>
+        <div>
+          <label className={styles.label}>Is it organic?</label>
+          <div className={styles.radioButton}>
+            <label className={styles.label}>
+              <input
+                type="radio"
+                name="organic"
+                value="Yes"
+                checked={formik.values.organic === 'Yes'}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                style={{ width: 'auto' }}
+              />
+              Yes
+            </label>
+            <label className={styles.label}>
+              <input
+                type="radio"
+                name="organic"
+                value="No"
+                checked={formik.values.organic === 'No'}
+                onChange={formik.handleChange}
+                style={{ width: 'auto' }}
+              />
+              No
+            </label>
+          </div>
+          {formik.touched.organic && formik.errors.organic ? (
+            <div style={{ color: 'red' }}>{formik.errors.organic}</div>
+          ) : null}
+        </div>
+        <div className={styles.submit}>
+          <button
+            type="submit"
+            style={{ background: '#206530', color: '#ffff' }}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
