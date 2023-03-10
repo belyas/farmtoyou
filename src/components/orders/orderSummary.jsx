1f5bf4330c8bfa14af139b11fdeaef7c3766f07e
Yun Ji <this.jiyun@gmail.com>
2023-03-10 09:51:29 +0100

    style orders page

--- src/components/orders/orderSummary.jsx
+++ src/components/orders/orderSummary.jsx
@@ -1,16 +1,31 @@
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import { Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
 
 const OrderSummary = ({ order }) => {
+  const createdDate = order.created_at.toString().slice(0, 10);
   return (
     <List>
-      <ListItem>{order.product_title}</ListItem>
-      <ListItem>Order ID:{order.id}</ListItem>
-      <ListItem>Created at:{order.created_at}</ListItem>
-      <ListItem>Product Price:{order.product_price}</ListItem>
-      <ListItem>Product Quantity:{order.product_quantity}</ListItem>
-      <ListItem>Delivery: {order.delivery_method}</ListItem>
-      <ListItem>Total:{order.total_price}</ListItem>
+      <ListItem>
+        <Typography variant="h5">Product {order.product_title}</Typography>
+      </ListItem>
+      <ListItem>
+        <Typography variant="body1">Order ID: {order.id}</Typography>
+      </ListItem>
+      <ListItem>
+        <Typography variant="body1">Created at: {createdDate}</Typography>
+      </ListItem>
+      <ListItem>
+        <Typography variant="body1">Product Price: € {order.product_price}</Typography>
+      </ListItem>
+      <ListItem>
+        <Typography variant="body1">Product Quantity: {order.product_quantity}</Typography>
+      </ListItem>
+      <ListItem>
+        <Typography variant="body1">Sub Total: {order.total_price}</Typography>
+      </ListItem>
+      <Divider />
     </List>
   );
 };
