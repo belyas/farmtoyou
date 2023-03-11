c3fcce9049e439f4f8cf5091653ab2a95f62b9b8
nimrashahzadi05 <nimra.shahzadi@outlook.com>
2023-03-11 13:14:20 +0100

    ¨test¨ç

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -262,7 +262,7 @@ export const Header = () => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
+console.log()
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
