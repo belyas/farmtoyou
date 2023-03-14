e5906cb66ac7f99f00e901beff51d236a94556db
nimrashahzadi05 <108905286+nimrashahzadi05@users.noreply.github.com>
2023-03-14 17:26:44 +0100

    Footer layout (#90)
    * footer icons
    
    * footer icons
    
    * footer done
    
    * footer
    
    * footer
    
    * footer done

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,76 +1,48 @@
 import { Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+
 import React from 'react';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import { Container } from '@mui/system';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import styles from '@/styles/Footer.module.css';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
-  },
-  {
-    title: 'Resources',
-    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
-];
+const footers = [];
 
 export const Footer = () => {
   return (
     <>
       <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
       <Container
-        maxWidth="md"
+        className={styles.footer}
+        maxWidth="xl"
         component="footer"
         sx={{
           borderTop: theme => `1px solid ${theme.palette.divider}`,
-          mt: 8,
-          py: [3, 6],
+          mt: 4,
+          py: [2, 4],
         }}
       >
         <Grid
           container
-          spacing={4}
-          justifyContent="space-evenly"
+          justifyContent="center"
         >
-          {footers.map(footer => (
-            <Grid
-              item
-              xs={6}
-              sm={3}
-              key={footer.title}
-            >
-              <Typography
-                variant="h6"
-                color="text.primary"
-                gutterBottom
-              >
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map(item => (
-                  <li key={item}>
-                    <Link
-                      href="#"
-                      variant="subtitle1"
-                      color="text.secondary"
-                    >
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
+          <Link>
+            <GitHubIcon
+              href="https://github.com/belyas/farmtoyou"
+              target="blank"
+            />
+          </Link>
+          <EmailIcon />
+          <FacebookIcon />
+          <TwitterIcon />
+          <InstagramIcon />
+          <Typography>&copy;FarmToYou,2023.All rights reserved.</Typography>
         </Grid>
       </Container>
     </>
