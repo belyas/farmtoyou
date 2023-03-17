0adec9cf321ec7224998fab4d89a300eede3656f
Yun Ji <this.jiyun@gmail.com>
2023-03-17 09:56:40 +0100

    attempt 2  to fix cors

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         // matching all API routes
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          // { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
