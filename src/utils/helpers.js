3a7d4dadf15498b28daf37e39057aaba1a4b6aef
yassine-belkaid <yassine.belkaid@adevinta.com>
2023-03-17 08:11:39 +0100

    chore: force to use vercel domain name online

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,6 @@
 export const getURL = () => {
   let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/';
+  url = url.indexOf('localhost') ? url : 'farmtoyou.vercel.app/';
   url = url.includes('http') ? url : `https://${url}`;
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
 
